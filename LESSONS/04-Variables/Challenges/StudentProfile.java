457bfb362509941ada1ed20e65929e747929c57d
Marius <mariussangwa@gmail.com>
2026-07-11 18:37:57 +0200

    Complete Lesson 04: Variables

---
+++ LESSONS/04-Variables/Challenges/StudentProfile.java
@@ -0,0 +1,24 @@
+
+
+public class StudentProfile {
+
+    public static void main(String[] args){
+        String name = "ISEZERANO Forever Hyacinthe";
+        int age = 17;
+        String university = "MIT";
+        String department = "Computer Sciences";
+        String level = "Advanced";
+        String country = "USA";
+
+        System.out.println("===== STUDENT PROFILE=====\n\n");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("University: "+ university);
+        System.out.println("Department: "+ department);
+        System.out.println("Level: "+ level);
+        System.out.println("Country: "+country);
+        
+    }
+    
+}     
+
