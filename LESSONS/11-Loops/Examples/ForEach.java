054919b0cf0d5e0162834c53adaa1ac1e8c7a0e2
Marius <mariussangwa@gmail.com>
2026-07-14 01:53:41 +0200

    inhanded for loop

---
+++ LESSONS/11-Loops/Examples/ForEach.java
@@ -0,0 +1,14 @@
+public class ForEach {
+    public static void main(String[] args){
+        String names[] = {
+            "Marius",
+            "Sangwa",
+            "Ezra",
+            "Muhirwa"
+        };
+        for (String name: names) {
+            System.out.println(name);
+            
+        }
+    }
+}
