ee8c5383c44941076752e6531afb18b3948ca399
Marius <mariussangwa@gmail.com>
2026-07-14 02:41:52 +0200

    Done one exercies

---
+++ LESSONS/11-Loops/Exercises/GuessGame.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.random.RandomGenerator;
+
+public class GuessGame {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        final int secret = 19;
+        int attempts =1;
+
+        System.out.println("\n=====WELCOME TO GUESS THE SECRET NUMBER =====\n");
+        System.out.print("Enter the number: ");
+
+        int value = input.nextInt();
+        while( value != secret){
+            if (value > secret){
+                System.out.println("Too high");
+            }else{
+                System.out.println("Too low");
+                
+            }
+            attempts++;
+            
+            System.out.print("Enter the number: ");
+            value = input.nextInt();
+
+
+        };
+
+        System.out.println("Congraturations! You guessed the numbers in " + attempts + " attempts");
+
+
+
+
+
+        
+    }
+}
