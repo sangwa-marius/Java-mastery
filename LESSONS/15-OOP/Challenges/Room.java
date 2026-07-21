fb70732bd423d9387dd70c37f9e06d99596cb09e
Marius <mariussangwa@gmail.com>
2026-07-21 23:16:10 +0200

    formatted some files

--- LESSONS/15-OOP/Challenges/Room.java
+++ LESSONS/15-OOP/Challenges/Room.java
@@ -1,27 +1,25 @@
 
-
 public class Room {
     private String roomName;
     private double size;
 
-    public Room(String roomName, double size){
-        if(roomName.isBlank()){
+    public Room(String roomName, double size) {
+        if (roomName.isBlank()) {
             System.err.println("The room name is required");
             return;
-        }else if(size <= 0){
+        } else if (size <= 0) {
             System.err.println("The room size should be greater than 0");
             return;
-        }else{
+        } else {
             this.roomName = roomName;
             this.size = size;
         }
     }
 
-    public void displayRoom(){
+    public void displayRoom() {
         System.out.println("\nRoom\n------");
         System.out.println(roomName);
-        System.out.println(size+"m²");
+        System.out.println(size + "m²");
     }
-
 
 }
