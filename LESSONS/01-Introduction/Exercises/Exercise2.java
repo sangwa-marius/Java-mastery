05d5db81cd76e3d339cdc8564bf6efd3f884e296
Marius <mariussangwa@gmail.com>
2026-07-22 20:59:45 +0200

    formatted some files

--- LESSONS/01-Introduction/Exercises/Exercise2.java
+++ LESSONS/01-Introduction/Exercises/Exercise2.java
@@ -7,7 +7,7 @@ Earth
 
 public class Exercise2 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Rwanda\nAfrica\nEarth"); // Notice the "\n" character for printing a new line?
     }
 }
