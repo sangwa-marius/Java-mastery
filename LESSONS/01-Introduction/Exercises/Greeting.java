85bb69d744fca32b74654ad52c5768e3db0b8338
Marius <mariussangwa@gmail.com>
2026-07-22 21:00:09 +0200

    formatted some files

--- LESSONS/01-Introduction/Exercises/Greeting.java
+++ LESSONS/01-Introduction/Exercises/Greeting.java
@@ -5,7 +5,7 @@ Welcome to Java Programming!
 
 public class Greeting {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Welcome to Java Programming!");
     }
 }
