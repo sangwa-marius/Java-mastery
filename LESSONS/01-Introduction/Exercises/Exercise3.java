49b4aeb4e667909499494f6da5c3353bf17b9cb5
Marius <mariussangwa@gmail.com>
2026-07-22 20:59:58 +0200

    formatted some files

--- LESSONS/01-Introduction/Exercises/Exercise3.java
+++ LESSONS/01-Introduction/Exercises/Exercise3.java
@@ -7,7 +7,7 @@ Print:
 
 public class Exercise3 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("*****\n*****\n*****");
     }
 }
