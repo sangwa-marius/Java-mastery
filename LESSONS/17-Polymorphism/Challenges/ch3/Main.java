bf58545fd11e2fa7f4d31af4f226029b97235218
Marius <mariussangwa@gmail.com>
2026-07-25 23:41:41 +0200

    formatted some files

--- LESSONS/17-Polymorphism/Challenges/ch3/Main.java
+++ LESSONS/17-Polymorphism/Challenges/ch3/Main.java
@@ -1,24 +1,22 @@
 
-
 public class Main {
-    public static void main(String[] args){
-        Switchable[] devices  = {
-            new SmartAirConditioner(),
-            new SmartLamp()
+    public static void main(String[] args) {
+        Switchable[] devices = {
+                new SmartAirConditioner(),
+                new SmartLamp()
         };
 
         EnergyMonitor[] monitored = {
-            new SmartAirConditioner(),
-            new SmartLamp()
+                new SmartAirConditioner(),
+                new SmartLamp()
         };
 
-        for (Switchable device : devices){
+        for (Switchable device : devices) {
             device.turnOn();
             device.turnOff();
         }
 
-
-        for (EnergyMonitor monitor: monitored){
+        for (EnergyMonitor monitor : monitored) {
             monitor.displayPowerUsage();
         }
     }
