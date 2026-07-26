ea3c7cf39342a433c888c771c4f5c0898c124732
Marius <mariussangwa@gmail.com>
2026-07-26 22:32:10 +0200

    formatted the Main file

--- LESSONS/17-Polymorphism/Challenges/bigch/Main.java
+++ LESSONS/17-Polymorphism/Challenges/bigch/Main.java
@@ -1,34 +1,33 @@
 
-
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Robot[] robots = {
-            new DeliveryRobot(1, "MariusTech"),
-            new CleaningRobot(2, "SamTech"),
-            new InspectionRobot(3, "Marius")
+                new DeliveryRobot(1, "MariusTech"),
+                new CleaningRobot(2, "SamTech"),
+                new InspectionRobot(3, "Marius")
         };
 
-        for(Robot robot : robots){
+        for (Robot robot : robots) {
             robot.displayInfo();
             robot.performTask();
             System.out.println();
         }
 
         Rechargeable[] rechargeables = {
-            new DeliveryRobot(1, "Marius"),
-            new CleaningRobot(2, "MariusTech")
+                new DeliveryRobot(1, "Marius"),
+                new CleaningRobot(2, "MariusTech")
         };
 
         WiFiEnable[] wiFiEnables = {
-            new DeliveryRobot(1, "Marius"),
-            new InspectionRobot(3, "mariusTech")
+                new DeliveryRobot(1, "Marius"),
+                new InspectionRobot(3, "mariusTech")
         };
 
-        for(Rechargeable rechargeable: rechargeables){
+        for (Rechargeable rechargeable : rechargeables) {
             rechargeable.recharge();
         }
 
-        for(WiFiEnable wiFiEnable : wiFiEnables){
+        for (WiFiEnable wiFiEnable : wiFiEnables) {
             wiFiEnable.connect();
         }
     }
