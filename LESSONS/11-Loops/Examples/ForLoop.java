6fc1d32994d184cd8821da9e9d2d7d9c7dab3c29
Marius <mariussangwa@gmail.com>
2026-07-29 22:23:07 +0200

    formatted the example files for the lesson 11

--- LESSONS/11-Loops/Examples/ForLoop.java
+++ LESSONS/11-Loops/Examples/ForLoop.java
@@ -1,9 +1,9 @@
-public class ForLoop{
-    public static void main(String[] args){
-        for (int i = 1; i <= 100; i++){
-            if(i % 2 == 0){
-              System.out.println("Even");
-            }else{
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Even");
+            } else {
                 System.out.println(i);
 
             }
--- LESSONS/11-Loops/Examples/NumberStatis.java
+++ LESSONS/11-Loops/Examples/NumberStatis.java
@@ -1,28 +1,28 @@
 import java.util.Scanner;
+
 public class NumberStatis {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("\nEnter 5 integers:\n");
-        int zeros=0, positives=0, negatives=0;
+        int zeros = 0, positives = 0, negatives = 0;
 
-        for (int i =1; i<=5; i++){
-            System.out.printf("Number %d: ",i);
+        for (int i = 1; i <= 5; i++) {
+            System.out.printf("Number %d: ", i);
             int value = input.nextInt();
-            if(value == 0){
+            if (value == 0) {
                 zeros++;
-            }else if( value < 0){
+            } else if (value < 0) {
                 negatives++;
-            }else{
+            } else {
                 positives++;
             }
 
         }
 
         System.out.println("\n===== Statistics =====");
-        System.out.println("Positive Numbers : "+ positives);
+        System.out.println("Positive Numbers : " + positives);
         System.out.println("Negative Numbers : " + negatives);
-        System.out.println("Zeros            : "+zeros);
-
+        System.out.println("Zeros            : " + zeros);
 
         input.close();
     }
