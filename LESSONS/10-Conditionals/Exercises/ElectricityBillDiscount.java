db4103e6182c268d4606e0a4ea05f05802e4f336
Marius <mariussangwa@gmail.com>
2026-07-29 22:19:04 +0200

    formatted the ElectricityBillDiscount.java

--- LESSONS/10-Conditionals/Exercises/ElectricityBillDiscount.java
+++ LESSONS/10-Conditionals/Exercises/ElectricityBillDiscount.java
@@ -28,8 +28,8 @@ Final bill
 
 import java.util.Scanner;
 
-public class ElectricityBillDiscount{
-    public static void main(String[] args){
+public class ElectricityBillDiscount {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.println("\n===== ELECTRICITY BILL DISCOUNT =====\n");
@@ -37,13 +37,12 @@ public class ElectricityBillDiscount{
         System.out.print("Enter total bill: ");
         double totalBill = input.nextDouble();
 
-        double discount =0, finalBill = totalBill;
+        double discount = 0, finalBill = totalBill;
 
-        if (totalBill > 100000){
-            discount = 0.15*totalBill;
+        if (totalBill > 100000) {
+            discount = 0.15 * totalBill;
             finalBill = totalBill - discount;
         }
-
 
         System.out.println("Original bill: " + totalBill);
         System.out.println("Discount: " + discount);
