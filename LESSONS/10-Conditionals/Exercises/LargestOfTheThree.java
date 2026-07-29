488655490761d330cf11f3ea551a84cfa85e46fb
Marius <mariussangwa@gmail.com>
2026-07-29 22:20:16 +0200

    formatted the LargestOfTheThree.java

--- LESSONS/10-Conditionals/Exercises/LargestOfTheThree.java
+++ LESSONS/10-Conditionals/Exercises/LargestOfTheThree.java
@@ -17,10 +17,10 @@ use arrays
 use Math.max()
 */
 
-import java.util.Scanner; 
+import java.util.Scanner;
 
-public class LargestOfTheThree{
-    public static void main(String[] args){
+public class LargestOfTheThree {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter first number: ");
@@ -32,13 +32,13 @@ public class LargestOfTheThree{
         System.out.print("Enter third number: ");
         double number3 = input.nextDouble();
 
-        if (number1 > number2 && number1 > number3){
+        if (number1 > number2 && number1 > number3) {
             System.out.println("The largest of the three is " + number1);
-        }else if( number2 > number1  && number2 > number3){
+        } else if (number2 > number1 && number2 > number3) {
             System.out.println("The largest of the three is " + number2);
-        }else if(number3 > number1 && number3 >number2){
+        } else if (number3 > number1 && number3 > number2) {
             System.out.println("The largest of the three is " + number3);
-        }else{
+        } else {
             System.out.println("All the numbers are equal");
         }
 
