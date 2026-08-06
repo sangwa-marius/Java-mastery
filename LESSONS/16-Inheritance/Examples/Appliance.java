1dd29c14638b157b50f6ffb3690d62de4d06da49
Marius <mariussangwa@gmail.com>
2026-08-06 19:05:37 +0200

    formatted the Appliance.java

--- LESSONS/16-Inheritance/Examples/Appliance.java
+++ LESSONS/16-Inheritance/Examples/Appliance.java
@@ -3,10 +3,9 @@ public class Appliance {
     protected String brand;
     protected int powerRating;
 
-    public Appliance(String brand , int powerRating){
+    public Appliance(String brand, int powerRating) {
         this.brand = brand;
         this.powerRating = powerRating;
     }
 
-    
 }
