07def45059a01b585da693b8dd64d76b9b1139ae
Marius <mariussangwa@gmail.com>
2026-08-08 23:13:53 +0200

    formatted Product.java

--- LESSONS/15-OOP/Examples/Product.java
+++ LESSONS/15-OOP/Examples/Product.java
@@ -3,42 +3,42 @@ public class Product {
     private double price;
     private int quantity;
 
-    public Product(String name, double  price, int quantity){
+    public Product(String name, double price, int quantity) {
         this.name = name;
-        this.price =  price;
+        this.price = price;
         this.quantity = quantity;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public double getPrice(){
+    public double getPrice() {
         return price;
     }
 
-    public int getQuantity(){
+    public int getQuantity() {
         return quantity;
     }
 
-    public void setPrice(double amount){
-        if(amount > 0)
-            price =amount;
+    public void setPrice(double amount) {
+        if (amount > 0)
+            price = amount;
         else
             System.out.println("invalid price.Price should be greater than 0");
-            return;
+        return;
     }
 
-    public void sellQuantity(int quantity){
-        if(quantity > this.quantity)
+    public void sellQuantity(int quantity) {
+        if (quantity > this.quantity)
             System.out.println("No enough quantity");
         else
-            this.quantity -=quantity;
+            this.quantity -= quantity;
     }
 
-    public void displayInfo(){
-        System.out.println("\nName     : "+name);
-        System.out.println("Price    : $"+price);
-        System.out.println("Quantity : "+quantity + "kg");
+    public void displayInfo() {
+        System.out.println("\nName     : " + name);
+        System.out.println("Price    : $" + price);
+        System.out.println("Quantity : " + quantity + "kg");
     }
 }
