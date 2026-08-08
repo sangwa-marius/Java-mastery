2417523d8dc83c33f04d118d8b5f6c68d9a1f9ad
Marius <mariussangwa@gmail.com>
2026-08-08 23:10:39 +0200

    formatted the ParameterDemo.java

--- LESSONS/12-Methods/Examples/ParameterDemo.java
+++ LESSONS/12-Methods/Examples/ParameterDemo.java
@@ -1,20 +1,20 @@
 public class ParameterDemo {
 
-    public static void greet(String name){
+    public static void greet(String name) {
         System.out.println("How are you " + name);
     }
 
-    public static void printSquare(int number){
-        System.out.println(number*number);
+    public static void printSquare(int number) {
+        System.out.println(number * number);
     }
 
-    public static void displayStudent(String name, int age){
+    public static void displayStudent(String name, int age) {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
     }
 
-    public static void printRectangle(int width, int length){
-        System.out.printf("The rectangle is %d units wide and %d units long",width, length);
+    public static void printRectangle(int width, int length) {
+        System.out.printf("The rectangle is %d units wide and %d units long", width, length);
     }
 
     public static void main(String[] args){
@@ -23,6 +23,5 @@ public class ParameterDemo {
         displayStudent("Marius", 18);
         printRectangle(34,65);
     }
-    
 
 }
