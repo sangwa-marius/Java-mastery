f8ea56d328cc9466fd9673fb5ad6668fd916d01e
Marius <mariussangwa@gmail.com>
2026-08-09 22:56:59 +0200

    formatted Exercise2.java

--- LESSONS/03-HelloWorld/Exercises/Exercise2.java
+++ LESSONS/03-HelloWorld/Exercises/Exercise2.java
@@ -10,7 +10,7 @@ using only print().
 
 public class Exercise2 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.print("Welcome ");
         System.out.print("to ");
         System.out.print("Java");
