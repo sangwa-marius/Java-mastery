ad569792b9180eb282b6d19fa9e00532744e76ab
Marius <mariussangwa@gmail.com>
2026-08-09 22:55:24 +0200

    formatted the Profile.java

--- LESSONS/03-HelloWorld/Challenges/Profile.java
+++ LESSONS/03-HelloWorld/Challenges/Profile.java
@@ -1,7 +1,6 @@
 
-
 public class Profile {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("=====================");
         System.out.println("      MY PROFILE     ");
         System.out.println("=====================");
