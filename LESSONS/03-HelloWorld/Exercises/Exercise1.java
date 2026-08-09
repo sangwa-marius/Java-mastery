1b05b032816f4738bc2046cce82071fde91a299b
Marius <mariussangwa@gmail.com>
2026-08-09 22:56:50 +0200

    formatted Exercise1.java

--- LESSONS/03-HelloWorld/Exercises/Exercise1.java
+++ LESSONS/03-HelloWorld/Exercises/Exercise1.java
@@ -9,7 +9,7 @@ using println().
 */
 
 public class Exercise1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Welcome\nto\nJava");
     }
 }
